4ba108f743f4a00c1757c954cbca2daed724e209
Jake Kidd <jcb.kdd@gmail.com>
2024-05-08 14:42:09 -0700

    function in index for connection

--- packages/adapters/indexer-db/src/index.ts
+++ packages/adapters/indexer-db/src/index.ts
@@ -2,7 +2,17 @@ import mongoose from "mongoose";
 
 export * from "./operations";
 
-// Connect to MongoDB.
-mongoose.connect("mongodb://localhost/app")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
+/**
+ * Connects to the Indexer MongoDB instance.
+ * This function establishes a connection to the MongoDB database specified by the connection string.
+ * It is designed to let any connection errors propagate, allowing higher-level handling or logging
+ * mechanisms to deal with them.
+ * 
+ * @param hostname - Address of the mongodb instance to connect to. Default: "mongodb://localhost/app"
+ *
+ * @throws {MongooseError} Throws an error if the connection fails. (This can include network issues,
+ * incorrect credentials, unauthorized access, or unavailable MongoDB service.)
+ */
+export async function connectIndexerDB(hostname?: string) {
+  await mongoose.connect(hostname ? hostname : "mongodb://localhost/app")
+}
