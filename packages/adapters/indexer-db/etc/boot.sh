7f8022302997724b29c4da7f431f1b4bbbe7cc96
Jake Kidd <jcb.kdd@gmail.com>
2024-05-08 14:54:26 -0700

    boot and install scripts

---
+++ packages/adapters/indexer-db/etc/boot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# boot.sh
+
+# Start MongoDB directly with a specified configuration file
+mongod --config ../mongodb.conf
--- packages/adapters/indexer-db/src/index.ts
+++ packages/adapters/indexer-db/src/index.ts
@@ -8,11 +8,11 @@ export * from "./operations";
  * It is designed to let any connection errors propagate, allowing higher-level handling or logging
  * mechanisms to deal with them.
  * 
- * @param hostname - Address of the mongodb instance to connect to. Default: "mongodb://localhost/app"
+ * @param hostname - Address of the mongodb instance to connect to. Default: "mongodb://localhost/indexer"
  *
  * @throws {MongooseError} Throws an error if the connection fails. (This can include network issues,
  * incorrect credentials, unauthorized access, or unavailable MongoDB service.)
  */
 export async function connectIndexerDB(hostname?: string) {
-  await mongoose.connect(hostname ? hostname : "mongodb://localhost/app")
+  await mongoose.connect(hostname ? hostname : "mongodb://localhost/indexer")
 }
