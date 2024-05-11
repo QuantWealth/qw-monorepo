b110ecf659f83b731f6d619a1f011feda146a47a
Yash-Garg <me@yashgarg.dev>
2024-05-11 22:37:05 +0530

    feat: setup swagger for API doc

--- packages/agents/nova/src/main.ts
+++ packages/agents/nova/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nova API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -14,6 +23,7 @@ async function bootstrap() {
       validationError: { target: true, value: true },
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
