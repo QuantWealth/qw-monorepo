477f30cfc50c5dcdfb9e87068f63fc2a8a7bd25b
Jake Kidd <jcb.kdd@gmail.com>
2024-05-29 07:04:41 -0700

    update controller and app for nest

--- packages/agents/orderbook/src/server/order.controller.ts
+++ packages/agents/orderbook/src/server/order.controller.ts
@@ -1,47 +1,47 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
-import { OrderService } from './order.service';
-import { IOrder } from './schema';
+import { Controller, Get, Post, Param, Body } from "@nestjs/common";
+import { OrderService } from "./order.service";
+import { IOrder } from "./schema";
 
-@Controller('orders')
+@Controller("orders")
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
-  @Get('user/:wallet')
-  getUserOrders(@Param('wallet') wallet: string) {
+  @Get("user/:wallet")
+  getUserOrders(@Param("wallet") wallet: string) {
     return this.orderService.getUserOrders(wallet);
   }
 
-  @Get('signer/:signer')
-  getUserOrdersBySigner(@Param('signer') signer: string) {
+  @Get("signer/:signer")
+  getUserOrdersBySigner(@Param("signer") signer: string) {
     return this.orderService.getUserOrdersBySigner(signer);
   }
 
-  @Post('submit')
+  @Post("submit")
   submitOrder(@Body() orderData: IOrder) {
     return this.orderService.submitOrder(orderData);
   }
 
-  @Post('execute/:id')
-  executeOrder(@Param('id') id: string) {
+  @Post("execute/:id")
+  executeOrder(@Param("id") id: string) {
     return this.orderService.executeOrder(id);
   }
 
-  @Post('cancel/:id')
-  cancelOrder(@Param('id') id: string) {
+  @Post("cancel/:id")
+  cancelOrder(@Param("id") id: string) {
     return this.orderService.cancelOrder(id);
   }
 
-  @Get(':id')
-  getOrder(@Param('id') id: string) {
+  @Get(":id")
+  getOrder(@Param("id") id: string) {
     return this.orderService.getOrder(id);
   }
 
   @Get()
   getOrders(
-    @Param('start') start: number,
-    @Param('end') end: number,
-    @Param('status') status?: string,
-    @Param('distribution') distribution?: boolean
+    @Param("start") start: number,
+    @Param("end") end: number,
+    @Param("status") status?: string,
+    @Param("distribution") distribution?: boolean
   ) {
     return this.orderService.getOrders(start, end, status, distribution);
   }
