f78f7a3cb6c27e53e56d12cad0b62e95e53316d6
sanchaymittal <sanchaymittal@gmail.com>
2024-05-31 14:42:09 +0400

    chore: comment out the sudo code

--- packages/agents/orderbook/src/index.ts
+++ packages/agents/orderbook/src/index.ts
@@ -1,7 +1,9 @@
+import {connectOrderbookDB} from "qw-orderbook-db";
 import { bootstrap } from './server';
 import { loadConfig } from './config';
 import { startPolling } from './poller';
 
+// Setup the server
 const setup = async () => {
   // Load the configuration
   const config = loadConfig();
@@ -19,3 +21,11 @@ setup().catch((error) => {
   console.error('Error setting up Orderbook server.', error);
   process.exit(1);
 });
+
+
+const setupDatabase = async () => {
+  // Setup the database
+  // ...
+
+  const database = connectOrderbookDB();
+}
