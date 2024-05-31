ee0fd76ace766b4f5b2c32d70a55c3baf8450350
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-05-31 22:29:40 +0530

    refactor: changed the description of query parameter

--- packages/agents/nova/src/core/resources/saving/dto/saving-apy-query.dto.ts
+++ packages/agents/nova/src/core/resources/saving/dto/saving-apy-query.dto.ts
@@ -11,7 +11,7 @@ export class SavingApyQueryDto {
   @IsNotEmpty()
   @Length(42, 42)
   @ApiProperty({
-    description: 'The wallet address',
+    description: 'The SCW address',
     example: '0x0617b72940f105811F251967EE4fdD5E38f159d5',
   })
   scwAddress: string;
