cf9dbcb680b888e0f8bbe0481a2981f2a89af3fd
sanchaymittal <sanchaymittal@gmail.com>
2024-06-01 22:53:37 +0400

    fix: schema structure

--- packages/adapters/orderbook-db/src/schema.ts
+++ packages/adapters/orderbook-db/src/schema.ts
@@ -1,33 +1,15 @@
 import mongoose from "mongoose";
+import { Type, Static } from "@sinclair/typebox";
 
 /// Order Schema
-/// TODO: amounts, dapps, signatures, stratrgyType need to be in a object[] 
-interface IOrder {
-  id: string;
-  signer: string; // signer address
-  wallet: string; // SCW wallet address
-  timestamps: {
-    placed: number;
-    executed?: number;
-    cancelled?: number;
-  };
-  dapps: string[];
-  distribution?: boolean;
-  amounts: string[];
-  signatures?: string[];
-  status: "P" | "E" | "C"; // Pending, Executed, Canceled
-  hashes?: string[]; // Optional array of transaction hashes
-  strategyType: "FLEXI" | "FIXED";
-}
-
-const OrderSchema = new mongoose.Schema<IOrder>({
+const OrderSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   signer: { type: String, required: true },
   wallet: { type: String, required: true },
   timestamps: {
     placed: { type: Number, required: true },
     executed: { type: Number },
-    cancelled: { type: Number }
+    cancelled: { type: Number },
   },
   dapps: [{ type: String, required: true }],
   distribution: { type: Boolean, required: true },
@@ -37,6 +19,8 @@ const OrderSchema = new mongoose.Schema<IOrder>({
   hashes: [{ type: String }],
   strategyType: { type: String, enum: ["FLEXI", "FIXED"], required: true },
 });
+
+type IOrder = typeof OrderSchema;
 
 // Pre-save middleware to set the order ID.
 OrderSchema.pre("save", function (next) {
@@ -54,14 +38,7 @@ OrderSchema.pre("save", function (next) {
 });
 
 /// User Schema
-interface IUser {
-  id: string;
-  wallet: string;
-  network: string;
-  deployed: boolean;
-  providers: string[];
-}
-const UserSchema = new mongoose.Schema<IUser>(
+const UserSchema = new mongoose.Schema(
   {
     id: { type: String, required: true, unique: true },
     wallet: { type: String, required: true },
@@ -71,6 +48,8 @@ const UserSchema = new mongoose.Schema<IUser>(
   },
   { timestamps: true }
 );
+
+type IUser = typeof UserSchema;
 
 /// Models
 const OrderModel = mongoose.model<IOrder>("Order", OrderSchema);
