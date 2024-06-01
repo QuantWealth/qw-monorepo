6eb368e66ccf1402d5fb177cd4ad6dc1b2fe9235
sanchaymittal <sanchaymittal@gmail.com>
2024-06-01 13:55:10 +0400

    feat: setup config at nova

---
+++ packages/agents/nova/src/config/index.ts
@@ -0,0 +1,75 @@
+import * as fs from 'fs';
+import Ajv from 'ajv';
+import { config as dotEnvConfig } from 'dotenv';
+import { Type, Static } from '@sinclair/typebox';
+import { NovaConfig, NovaConfigSchema } from './schema';
+
+// Load environment variables
+dotEnvConfig();
+
+
+export const getEnvConfig = (): NovaConfig => {
+  let configJson: Record<string, any> = {};
+  let configFile: any = {};
+
+  try {
+    configJson = JSON.parse(process.env.NOVA_CONFIG || '');
+  } catch (e: unknown) {
+    console.info(
+      'No NOVA_CONFIG exists; using config file and individual env vars.',
+    );
+  }
+
+  try {
+    const path = process.env.NOVA_CONFIG_FILE ?? 'config.json';
+    if (fs.existsSync(path)) {
+      const json = fs.readFileSync(path, { encoding: 'utf-8' });
+      configFile = JSON.parse(json);
+    }
+  } catch (e: unknown) {
+    console.error('Error reading config file!');
+    process.exit(1);
+  }
+
+  const novaConfig: NovaConfig = {
+    chains: process.env.NOVA_CHAIN_CONFIG
+      ? JSON.parse(process.env.NOVA_CHAIN_CONFIG)
+      : configJson.chains || configFile.chains,
+    logLevel:
+      process.env.NOVA_LOG_LEVEL ||
+      configJson.logLevel ||
+      configFile.logLevel ||
+      'info',
+    environment:
+      process.env.NOVA_ENVIRONMENT ||
+      configJson.environment ||
+      configFile.environment ||
+      'staging',
+  };
+
+  const ajv = new Ajv();
+  const validate = ajv.compile(NovaConfigSchema);
+  const valid = validate(novaConfig);
+  if (!valid) {
+    throw new Error(
+      validate.errors
+        ?.map((err: any) => JSON.stringify(err, null, 2))
+        .join(','),
+    );
+  }
+
+  return novaConfig;
+};
+
+export let novaConfig: NovaConfig | undefined;
+
+/**
+ * Gets and validates the nova config from the environment.
+ * @returns The nova config with sensible defaults
+ */
+export const getConfig = async (): Promise<NovaConfig> => {
+  if (!novaConfig) {
+    novaConfig = getEnvConfig();
+  }
+  return novaConfig;
+};
---
+++ packages/agents/nova/src/config/schema.ts
@@ -0,0 +1,39 @@
+import { Type, Static } from '@sinclair/typebox';
+import { TAddress, TLogLevel } from '@qw/utils';
+import { Provider } from 'ethers';
+
+// Define the TChainConfig schema
+export const TChainConfig = Type.Object({
+  providers: Type.Array(Type.String()),
+  contractAddresses: Type.Object({
+    QWManager: Type.Object({
+      contractName: Type.String(),
+      address: TAddress,
+    }),
+    QWRegistry: Type.Object({
+      contractName: Type.String(),
+      address: TAddress,
+    }),
+    QWAaveV3: Type.Object({
+      contractName: Type.String(),
+      address: TAddress,
+    }),
+  }),
+});
+
+// Define the TRPCConfig schema
+export const TRPCConfig = Type.Object({
+  url: Type.String(),
+});
+
+// Define the NovaConfigSchema
+export const NovaConfigSchema = Type.Object({
+  chains: Type.Record(Type.String(), TChainConfig),
+  logLevel: TLogLevel,
+  environment: Type.Union([
+    Type.Literal('staging'),
+    Type.Literal('production'),
+  ]),
+});
+
+export type NovaConfig = Static<typeof NovaConfigSchema>;
