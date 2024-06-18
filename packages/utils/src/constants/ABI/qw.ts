1a951275da433a05d7d86a5f5fb46ee6ef8bf0fc
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-06-18 09:29:00 +0530

    fix: resolved qw_manager abi

--- packages/utils/src/constants/ABI/qw.ts
+++ packages/utils/src/constants/ABI/qw.ts
@@ -1,27 +1,26 @@
 export const QW_MANAGER_ABI = [
-    // Constructor
-    "constructor()",
-  
-    // View functions
-    "function REGISTRY() view returns (address)",
-    "function owner() view returns (address)",
-  
-    // Non-payable functions
-    "function close(address[] _targetQwChild, bytes[] _callData) nonpayable",
-    "function execute(address[] _targetQwChild, bytes[] _callData, address _tokenAddress, uint256 _amount) nonpayable",
-    "function receiveFunds(address user, address _tokenAddress, uint256 _amount) nonpayable",
-    "function renounceOwnership() nonpayable",
-    "function transferOwnership(address newOwner) nonpayable",
-    "function withdraw(address user, address _tokenAddress, uint256 _amount) nonpayable",
-  
-    // Events
-    "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
-  
-    // Errors
-    "error CallFailed()",
-    "error ContractNotWhitelisted()",
-    "error InvalidInputLength()",
-    "error OwnableInvalidOwner(address owner)",
-    "error OwnableUnauthorizedAccount(address account)"
-  ];
-  
+  // Constructor
+  "constructor()",
+
+  // View functions
+  "function REGISTRY() view returns (address)",
+  "function owner() view returns (address)",
+
+  // Non-payable functions
+  "function close(address[] _targetQwChild, bytes[] _callData)",
+  "function execute(address[] _targetQwChild, bytes[] _callData, address _tokenAddress, uint256 _amount)",
+  "function receiveFunds(address user, address _tokenAddress, uint256 _amount)",
+  "function renounceOwnership()",
+  "function transferOwnership(address newOwner)",
+  "function withdraw(address user, address _tokenAddress, uint256 _amount)",
+
+  // Events
+  "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
+
+  // Errors
+  "error CallFailed()",
+  "error ContractNotWhitelisted()",
+  "error InvalidInputLength()",
+  "error OwnableInvalidOwner(address owner)",
+  "error OwnableUnauthorizedAccount(address account)"
+];
